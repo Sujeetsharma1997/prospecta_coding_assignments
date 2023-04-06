e99a4a0afaed7016c1e10f4595e4f6fd3947520e
sujeet kumar <105916611+Sujeetsharma1997@users.noreply.github.com>
2023-04-06 12:25:32 +0530

    init

--- prospecta_coding_assignments/src/main/java/com/prospecta/data/JSON_Data.java
+++ prospecta_coding_assignments/src/main/java/com/prospecta/data/JSON_Data.java
@@ -13,26 +13,26 @@ public class JSON_Data {
 		super();
 	}
 
-//	public JSON_Data(String count, List<Entry> entries) {
-//		super();
-//		this.count = count;
-//		this.entries = entries;
-//	}
-//
-//	public String getCount() {
-//		return count;
-//	}
-//
-//	public void setCount(String count) {
-//		this.count = count;
-//	}
-//
-//	public List<Entry> getEntries() {
-//		return entries;
-//	}
-//
-//	public void setEntries(List<Entry> entries) {
-//		this.entries = entries;
-//	}
-//	
+	public JSON_Data(String count, List<Entry> entries) {
+		super();
+		this.count = count;
+		this.entries = entries;
+	}
+
+	public String getCount() {
+		return count;
+	}
+
+	public void setCount(String count) {
+		this.count = count;
+	}
+
+	public List<Entry> getEntries() {
+		return entries;
+	}
+
+	public void setEntries(List<Entry> entries) {
+		this.entries = entries;
+	}
+	
 }
